54f9a75ed8d7c00dfaea821b81f202168f466160
tamago3keran <yamataku3831@gmail.com>
2020-01-12 01:27:00 +0900

    add office show page

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -1,16 +1,27 @@
 class OfficesController < ApplicationController
-  before_action :load_resource, only: [:new]
+  before_action :load_resource, only: [:show, :new]
+
+  def show
+    redirect_to new_office_path if @office.blank?
+  end
 
   def new; end
 
   def create
     location = Google::Geocoder.location_search(office_params[:address])
-    Office.create(address: office_params[:address], latitude: location["lat"], longitude: location["lng"])
+    office = Office.new(address: office_params[:address], latitude: location["lat"], longitude: location["lng"])
+    if office.save
+      redirect_to office_path
+    else
+      render :new
+    end
   end
 
   private
     def load_resource
       case params[:action].to_sym
+      when :show
+        @office = Office.first
       when :new
         @office = Office.new
       end
