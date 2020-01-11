1ab819f848b11d0b1fccb68eae95209b1369e3ba
tamago3keran <yamataku3831@gmail.com>
2020-01-12 01:28:29 +0900

    add validation for office model

--- app/models/office.rb
+++ app/models/office.rb
@@ -5,4 +5,8 @@ class Office
   field :address, type: String
   field :latitude, type: Float
   field :longitude, type: Float
+
+  validates :address, presence: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
 end
